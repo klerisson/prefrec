3e706493ac3db32cb27a1172e1724326e4570c59
cricia <cricia@gmail.com>
2014-11-18 22:25:38 -0200

    cluster parameter changed

--- prefrec-execution/src/main/java/br/ufu/facom/lsi/prefrec/execution/crossvalidation/CrossValidation.java
+++ prefrec-execution/src/main/java/br/ufu/facom/lsi/prefrec/execution/crossvalidation/CrossValidation.java
@@ -6,6 +6,7 @@ package br.ufu.facom.lsi.prefrec.execution.crossvalidation;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.HashMap;
